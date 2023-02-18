7100ac3680870c8eb95d7b6ed0df738d151ceab4
Hugo Boueix <hugo.boueix@outlook.com>
2023-02-18 18:10:04 +0100

    :label: add types to methods

--- tests/test_DataDirectory.py
+++ tests/test_DataDirectory.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from typing import Any
 
 from datadir import DataDirectory
 
@@ -9,26 +10,26 @@ class TestDataDirectory:
     basedir = 'test_basedir'
     not_existing_dir = 'not_existing_dir'
 
-    def setup_method(self, method):
+    def setup_method(self, method: Any) -> None:
         os.makedirs(TestDataDirectory.basedir, exist_ok=False)
 
-    def teardown_method(self, method):
+    def teardown_method(self, method: Any) -> None:
         shutil.rmtree(TestDataDirectory.basedir)
         if os.path.exists(TestDataDirectory.not_existing_dir):
             shutil.rmtree(TestDataDirectory.not_existing_dir)
 
-    def test_create_basedir_if_not_exists(self):
+    def test_create_basedir_if_not_exists(self) -> None:
         data_dir = DataDirectory(TestDataDirectory.not_existing_dir)
 
         assert os.path.isdir(TestDataDirectory.not_existing_dir)
 
-    def test_create_basedir_if_exists(self):
+    def test_create_basedir_if_exists(self) -> None:
         try:
             data_dir = DataDirectory(TestDataDirectory.basedir)
         except FileExistsError:
             assert False, 'create_basedir() should not raise FileExistsError'
 
-    def test_create_subdir_if_not_exists(self):
+    def test_create_subdir_if_not_exists(self) -> None:
         data_dir = DataDirectory(TestDataDirectory.basedir)
 
         subdir = TestDataDirectory.not_existing_dir
@@ -36,7 +37,7 @@ class TestDataDirectory:
 
         assert os.path.isdir(os.path.join(TestDataDirectory.basedir, subdir))
 
-    def test_create_subdir_if_exists(self):
+    def test_create_subdir_if_exists(self) -> None:
         data_dir = DataDirectory(TestDataDirectory.basedir)
         subdir = 'existing_subdir'
 
@@ -47,13 +48,13 @@ class TestDataDirectory:
         except FileExistsError:
             assert False, 'create_subdir() should not raise FileExistsError'
 
-    def test_subdir_exists_if_not_exists(self):
+    def test_subdir_exists_if_not_exists(self) -> None:
         data_dir = DataDirectory(TestDataDirectory.basedir)
         subdir = TestDataDirectory.not_existing_dir
 
         assert data_dir.subdir_exists(subdir) is False
 
-    def test_subdir_exists_if_exists(self):
+    def test_subdir_exists_if_exists(self) -> None:
         data_dir = DataDirectory(TestDataDirectory.basedir)
         subdir = 'existing_subdir'
 
