86e3907cb9ad7d72355cf91792e709eaea15ad7f
Hugo Boueix <hugo.boueix@outlook.com>
2023-02-18 17:37:23 +0100

    :white_check_mark: pass test create_basedir if exists

--- datadir/datadirectory.py
+++ datadir/datadirectory.py
@@ -5,7 +5,7 @@ class DataDirectory:
 
     def __init__(self, basedir_path: str) -> None:
         self.basedir_path = basedir_path
-        self.create_basedir()
+        self.create_basedir(exist_ok=True)
 
     def create_basedir(self, exist_ok: bool = False) -> None:
         os.makedirs(self.basedir_path, exist_ok=exist_ok)
