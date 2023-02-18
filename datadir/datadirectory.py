dcde91c5e6ee1116c989dd1d5acb6f868349426a
Hugo Boueix <hugo.boueix@outlook.com>
2023-02-18 17:52:07 +0100

    :white_check_mark: pass test create_subdir if exists

--- datadir/datadirectory.py
+++ datadir/datadirectory.py
@@ -10,6 +10,6 @@ class DataDirectory:
     def create_basedir(self, exist_ok: bool = False) -> None:
         os.makedirs(self.basedir_path, exist_ok=exist_ok)
 
-    def create_subdir(self, subdir_path: str) -> None:
+    def create_subdir(self, subdir_path: str, exist_ok: bool = True) -> None:
         subdir_path = os.path.join(self.basedir_path, subdir_path)
-        os.makedirs(subdir_path)
+        os.makedirs(subdir_path, exist_ok=exist_ok)
