d66aa820e7d17fd040e1de6201d58dad03496434
Hugo Boueix <hugo.boueix@outlook.com>
2023-02-19 14:22:38 +0100

    :white_check_mark: pass test save_text_file if invalid mode

--- tests/test_DataDirectory.py
+++ tests/test_DataDirectory.py
@@ -223,3 +223,11 @@ class TestDataDirectory:
 
         with open(os.path.join(TestDataDirectory.basedir, file), 'r') as f:
             assert f.read().splitlines(keepends=True) == text + text
+
+    def test_save_text_file_if_invalid_mode(self) -> None:
+        data_dir = DataDirectory(TestDataDirectory.basedir)
+        file = 'file'
+        text = 'test\nmultilines'
+
+        with pytest.raises(ValueError):
+            data_dir.save_text_file(file, text, mode='invalid_mode')
