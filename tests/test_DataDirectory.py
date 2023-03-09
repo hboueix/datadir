728ad3b137099f56d9f7ea169c3c2305782940c5
Hugo Boueix <hugo.boueix@outlook.com>
2023-03-09 16:10:17 +0100

    :white_check_mark: pass test get_df if parquet path

--- tests/test_DataDirectory.py
+++ tests/test_DataDirectory.py
@@ -270,6 +270,16 @@ class TestDataDirectory:
 
         assert df.equals(df2)
 
+    def test_get_df_if_parquet_path(self) -> None:
+        data_dir = DataDirectory(TestDataDirectory.basedir)
+        file = 'file.parquet'
+        df = pd.DataFrame({'col1': [1, 2], 'col2': ['a', 'b']})
+        df.to_parquet(os.path.join(TestDataDirectory.basedir, file), index=False)
+
+        df2 = data_dir.get_df(file)
+
+        assert df.equals(df2)
+
     def test_save_df_if_csv_path(self) -> None:
         data_dir = DataDirectory(TestDataDirectory.basedir)
         file = 'file.csv'
