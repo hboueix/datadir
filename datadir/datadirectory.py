3daec67edaaddc72f28a45691d642a797c25028a
Hugo Boueix <hugo.boueix@outlook.com>
2023-10-18 14:15:48 +0200

    :fire: move create_basedir into init method

--- datadir/datadirectory.py
+++ datadir/datadirectory.py
@@ -11,9 +11,6 @@ class DataDirectory:
 
     def __init__(self, basedir_path: str, exist_ok: bool = True) -> None:
         self.basedir_path = basedir_path
-        self.create_basedir(exist_ok=exist_ok)
-
-    def create_basedir(self, exist_ok: bool = True) -> None:
         os.makedirs(self.basedir_path, exist_ok=exist_ok)
 
     def create_subdir(self, subdir_path: str, exist_ok: bool = True) -> None:
